f402903d512111a328f8d06a3a7dcae3749860c7
Nomusa Mtshali <kelebogilemataboge17@gmail.com git config --global user.email kelebogilemataboge17@gmail.com>
2024-03-26 16:40:59 +0200

    songs and genre commit

--- index.js
+++ index.js
@@ -5,32 +5,33 @@ const songs = [
   { title: "I Want You Back", artist: "The Jackson 5", genre: "Pop" },
   { title: "Spirit in the Sky", artist: "Norman Greenbaum", genre: "Rock" },
   { title: "Cherry Bomb", artist: "The Runaways", genre: "Rock" },
-  {
-    title: "Escape (The Piña Colada Song)",
-    artist: "Rupert Holmes",
-    genre: "Pop",
-  },
+  { title: "Escape (The Piña Colada Song)", artist: "Rupert Holmes", genre: "Pop", },
   { title: "O-O-H Child", artist: "The Five Stairsteps", genre: "R&B" },
-  {
-    title: "Ain't No Mountain High Enough",
-    artist: "Marvin Gaye & Tammi Terrell",
-    genre: "R&B",
-  },
+  { title: "Ain't No Mountain High Enough", artist: "Marvin Gaye & Tammi Terrell", genre: "R&B", },
   { title: "Come and Get Your Love", artist: "Redbone", genre: "Rock" },
   { title: "I'm Not in Love", artist: "10cc", genre: "Pop" },
-  {
-    title: "Fooled Around and Fell in Love",
-    artist: "Elvin Bishop",
-    genre: "Rock",
-  },
+  { title: "Fooled Around and Fell in Love", artist: "Elvin Bishop", genre: "Rock", },
+  
   // Feel free to add even more songs
+  { title: "90s by Nature", artist: "Showtek", genre: "Electro-House" },
+  { title: "Enter Sandman", artist: "Metallica", genre: "Metal" },
+  { title: "Lover Is a Day", artist: "Cuco", genre: "Lo-fi" },
+  { title: "Blame", artist: "Calvin Harris, John Newman", genre: "Electro-House" },
+  { title: "No More Tears", artist: "Ozzy Osbourne", genre: "Metal" },
+  { title: "Affection", artist: "Jinsang", genre: "Lo-fi" },
+  { title: "Saxo", artist: "Aniello", genre: "Electro-House" },
+  { title: "Thunderstruck", artist: "AC/DC", genre: "Metal" },
+  { title: "Sorry, I Like You", artist: "Burbank", genre: "Lo-fi" }
 ];
 
 // Object containing each Guardian's preferred genre
 const guardians = {
   "Star-Lord": "Rock",
-  Gamora: "Pop",
+  "Gamora": "Pop",
   // Add preferences for Drax, Rocket, and Groot
+  "Drax": "Lo-fi",
+  "Rocket": "Metal",
+  "Groot": "Electro-House",
 };
 
 // Function to generate playlist based on preferred genre
